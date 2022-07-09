e62e311bdbeec734064bc87c62e972ebf1fe3751
Dmitry_Drobyshev <ddi1981@mail.ru>
2022-07-09 12:05:05 +0300

    homework task1 ready

--- Tasks/Drobyshev/Task1/task1.py
+++ Tasks/Drobyshev/Task1/task1.py
@@ -1,1 +1,14 @@
-print('Hello future IT millionaires!')
+import math
+print ('In our DROB-Bank you will get the best deposit offer, check it yourself by entering the initial data below.')
+
+x = int(input ('Please enter the deposit amount, BYN: \n'))
+y = int(input ('Please enter the deposit term, years: \n'))
+z = float(input ('Please enter the annual interest rate, % : \n'))
+m = y * 12
+t = x * (1 + z/100/12) ** m
+p = t - x
+d = ((p/x)*100)/y
+
+print ('The amount =', int(t) , 'BYN' )
+print ('Profit =' , int(p) ,'BYN')
+print ('The yield was (as a percentage per annum):' , round(d,2) , '%')
