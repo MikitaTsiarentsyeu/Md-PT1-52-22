5a8deca88b50b0734f8266acc890b64d209ecee4
Dmitry_Drobyshev <ddi1981@mail.ru>
2022-07-10 11:20:58 +0300

    Update Task1.py

--- Tasks/Drobyshev/Task1/task1.py
+++ Tasks/Drobyshev/Task1/task1.py
@@ -1,14 +1,18 @@
-import math
-print ('In our DROB-Bank you will get the best deposit offer, check it yourself by entering the initial data below.')
+print ('Deposit: initial amount - 20000 BYR; term - 5 years; interest (annual) - 15%; !monthly capitalization! '
 
-x = int(input ('Please enter the deposit amount, BYN: \n'))
-y = int(input ('Please enter the deposit term, years: \n'))
+'Calculate the amount on the account at the end of the specified period.') 
+print ('Answer: The amount = 42143.63 BYN')
+
+print ('To check my solution, as well as to calculate the compound interest using your data, use the form below.')
+
+x = float(input ('Please enter the deposit amount, BYN: \n'))
+y = float(input ('Please enter the deposit term, years: \n'))
 z = float(input ('Please enter the annual interest rate, % : \n'))
 m = y * 12
 t = x * (1 + z/100/12) ** m
 p = t - x
 d = ((p/x)*100)/y
 
-print ('The amount =', int(t) , 'BYN' )
-print ('Profit =' , int(p) ,'BYN')
+print ('The amount =', round(t,2) , 'BYN' )
+print ('Profit =' , round(p,2) ,'BYN')
 print ('The yield was (as a percentage per annum):' , round(d,2) , '%')
