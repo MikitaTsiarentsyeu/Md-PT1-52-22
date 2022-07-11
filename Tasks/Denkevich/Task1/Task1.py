aecd70318eb784e8a36f25e3c78873a49b027cc1
EugeneDenkevich <106911093+EugeneDenkevich@users.noreply.github.com>
2022-07-11 21:54:23 +0300

    Correct Task1.py

--- Tasks/Denkevich/Task1/Task1.py
+++ Tasks/Denkevich/Task1/Task1.py
@@ -1,24 +1,33 @@
-# Без ввода данных без math:
-#---------------------------
+# Without entering data and importing math:
+# ---------------------------
+import math
 print('\n')
 sum = 20000
 t = 5
 per = 15
 
-D = sum * ((1 + (per/100)/12)**t)
-print('Рассчётное значение без ввода данных: %.2f BYN\n' % D)
+D = sum * ((1 + (per/100)/12)**(t*12))
+print('Calculated value is: %.2f BYN\n' % D)
 
-# С вводом данных и с math:
-#--------------------------
-import math
+# With entering data and importing math:
+# --------------------------
 
-sum = input('Введите сумму депозита: \n')
-t = input('Введите срок: \n')
-per = input('Введите процент: \n')
+sum = input('Enter the sum of the deposit: \n')
+t = input('Enter the deposit term: \n')
+per = input('Enter the percent: \n')
+val = input('Would you like to capitalize your deposit monthly? (y/n): \n')
 
 sum = int(sum)
 t = int(t)
 per = int(per)
-D = sum * math.pow((1 + (per/100)/12), t)
-print('Сумма депозита по окончании срока: %.2f BYN' % D)
-print('\n')
+
+if (val == 'y'):
+    D = sum * math.pow((1 + (per/100)/12), (t*12))
+    print('Calculated value of the deposit is: %.2f BYN' % D)
+    print('\n')
+elif (val == 'n'):
+    D = sum * (1 + (per/100*t))
+    print('Calculated value of the deposit is: %.2f BYN' % D)
+    print('\n')
+else:
+    print('Incorrectly! Reload the program.')
