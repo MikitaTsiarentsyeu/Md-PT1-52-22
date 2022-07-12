4701d7c9f62ff7e6854b88a8f95e6452b0f263e2
Cyril Bodyako <3867922@gmail.com>
2022-07-12 18:29:51 +0300

    Update Task1.py

--- Tasks/Bodyako/Task1/Task1.py
+++ Tasks/Bodyako/Task1/Task1.py
@@ -1,7 +1,7 @@
-a =int(input('1. Enter the amount of deposit (USD): '))
-b =int(input('2. Enter deposit interest rate (%): '))
-c =int(input('3. Enter the deposit term (yers): '))
-d =str(input('4. Choose the type of capitalization, type "month" or "year": '))
+a = int(input('1. Enter the amount of deposit (USD): '))
+b = int(input('2. Enter deposit interest rate (%): '))
+c = int(input('3. Enter the deposit term (yers): '))
+d = str(input('4. Choose the type of capitalization, type "month" or "year": '))
 
 if d == 'month': 
     x = a*((1+b/100/12)**(c*12))
