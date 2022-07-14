3e7d96d9e16982cabb9f350dc1c0e03233d05d9d
arinabard <arinaxxx5@gmail.com>
2022-07-14 12:32:18 +0300

    Update task1.py

--- Tasks/Baranovskaya/Task1/task1.py
+++ Tasks/Baranovskaya/Task1/task1.py
@@ -1,11 +1,18 @@
 import math
 
-dep = 20000 
-t = 5
-per = 15
+dep = float(input("Enter Principal, BYN:\n"))
+per = float(input("Enter Interest Rate, %:\n"))
+t = float(input("Enter Term, years:\n"))
 
-T = t*12 
-Per = per/100
-Sum = dep * math.pow(1 + Per/12, T)
-
-print(round(Sum, 2))
+c = int(input("Choose Compounding:\n 1) Monthly\n 2) Annualy\n"))
+if c == 1:
+   T = t*12 
+   Per = per/100
+   Sum = dep * math.pow(1 + Per/12, T)
+   print("Future value:\n", round(Sum, 2), "BYN")
+elif c == 2:
+   Per = per/100
+   Sum = dep * math.pow(1 + Per, t)
+   print("Future value:\n", round(Sum, 2), "BYN")
+else:
+ print("Invalid input format. Please, try again.\n") 
