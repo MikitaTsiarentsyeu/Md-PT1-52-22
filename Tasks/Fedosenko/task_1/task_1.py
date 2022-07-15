89214b1d151dc866fa9e780c19c55f51c8fa04ab
UlyanaFedosenko <375336249423@yandex.by>
2022-07-15 13:39:55 +0300

    update task_1.py

--- Tasks/Fedosenko/task_1/task_1.py
+++ Tasks/Fedosenko/task_1/task_1.py
@@ -1,12 +1,12 @@
 from decimal import *
 
-initial_amount =  20000
-term_in_years = 5
-interest_rate = Decimal('0.15')
+initial_amount = float(input('Please, enter the initial deposit amount: \n'))
+term_in_years = int(input('Please, enter the term of the deposit in years: \n'))
+interest_rate = float(input('Please, enter the interest rate: \n'))
 
 term_in_days = term_in_years * 365  
 
-deposit_income = (initial_amount*(1+interest_rate/365)**term_in_days)
+deposit_income = Decimal(initial_amount*(1+interest_rate/(365*100))**term_in_days)
 
-print (deposit_income)
+print (f'The deposit income will be: {deposit_income.quantize(Decimal("1.00"), ROUND_HALF_UP)} ')
 
