bbdb1d4be482cfb5b8458539c8960283f85a38e4
Maglait <maglait@mail.ru>
2022-07-20 20:57:10 +0300

    fast upd
    decimal

--- Tasks/Kulik/Task1/1.py
+++ Tasks/Kulik/Task1/1.py
@@ -1,5 +1,8 @@
-a = int(input("Deposit amount:\n")) 
-b = int(input('Years amount:\n'))
-c = int(input('Procent:\n'))
+import decimal
+
+
+a = decimal.Decimal(input("Deposit amount:\n")) 
+b = decimal.Decimal(input('Years amount:\n'))
+c = decimal.Decimal(input('Procent:\n'))
 x=a*((1+b/100/12)**(c*12))
-print (x)
+print (round(x,2))
