ff245a6e16dee4dfc52c52f00e96c1e27a51573e
Dmitry_Drobyshev <ddi1981@mail.ru>
2022-07-27 14:46:59 +0300

    task_lesson.py
    don't judge strictly)

---
+++ Tasks/Drobyshev/Task3/task_lesson.py
@@ -0,0 +1,10 @@
+"five thirteen two eleven seventeen two one thirteen ten four eight five nineteen"
+а = list(set({'five': 5, 'thirteen': 13, 'two':2, 'eleven': 11, 'seventeen':17, 'one':1, 'ten':10, 'four':4, 'eight':8, 'five':5, 'nineteen':19}.values()))
+print(а)
+for a in range (len(а)-1):
+    if a%2 == 0:
+        print (а[a]*а[a+1], end="  ")
+    else:
+        print (а[a]+а[a+1], end="  ")
+print()
+print(f"Полная сумма всех нечётных чисел = {sum(а for а in а if а%2 !=0)}")
