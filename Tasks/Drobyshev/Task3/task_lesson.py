c3bc9aad3b309deedf3c2461b3a5166e4092f287
Dmitry_Drobyshev <ddi1981@mail.ru>
2022-07-27 17:00:22 +0300

    Update task_lesson.py
    optimization, reduced the number of lines of code

--- Tasks/Drobyshev/Task3/task_lesson.py
+++ Tasks/Drobyshev/Task3/task_lesson.py
@@ -2,9 +2,6 @@
 а = list(set({'five': 5, 'thirteen': 13, 'two':2, 'eleven': 11, 'seventeen':17, 'one':1, 'ten':10, 'four':4, 'eight':8, 'five':5, 'nineteen':19}.values()))
 print(а)
 for a in range (len(а)-1):
-    if a%2 == 0:
-        print (а[a]*а[a+1], end="  ")
-    else:
-        print (а[a]+а[a+1], end="  ")
+    print (а[a]*а[a+1], end="  ") if a%2 == 0 else print (а[a]+а[a+1], end="  ")
 print()
 print(f"The total sum of all odd numbers = {sum(а for а in а if а%2 !=0)}")
