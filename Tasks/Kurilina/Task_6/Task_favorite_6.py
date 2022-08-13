3605f2b7f89fd882cf9a64ead7c51b10c0ab9a45
ViolettaKurilina <105584668+ViolettaKurilina@users.noreply.github.com>
2022-08-13 22:28:12 +0300

    Update Task_favorite_6.py

--- Tasks/Kurilina/Task_6/Task_favorite_6.py
+++ Tasks/Kurilina/Task_6/Task_favorite_6.py
@@ -1,2 +1,17 @@
+
+
 word = "FizzBuzz"
 print(*(word[(0,4,4)[x%3]:4 if x % 5 else 8] or x for x in range(1,101)))
+
+
+for i in range(1,101):
+    
+    final_word = ''
+    if i % 3 == 0:
+        final_word+=word[:4]
+    if i % 5 == 0:
+        final_word+=word[4:]
+    if not final_word:
+        print(i) 
+        continue
+    print(final_word)     
