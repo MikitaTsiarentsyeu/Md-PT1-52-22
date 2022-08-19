62a941bb9b6bf355601e6c1799ca2f427cd421b8
EugeneDenkevich <106911093+EugeneDenkevich@users.noreply.github.com>
2022-08-19 06:50:16 +0300

    Update2 Taks7.py

--- Tasks/Denkevich/Task7/Task7.py
+++ Tasks/Denkevich/Task7/Task7.py
@@ -19,18 +19,20 @@ print(sum_el(l))
 
 
 print('-----------')
-def fib(n, a = 0, b = 0, c = 1):
+
+
+def fib(n, a=0, b=0, c=1):
     if n == 0:
         print("There is no Fibonachi numbers.")
     elif n == 1:
-        print(f"{a}", end = '.')
+        print(f"{a}", end='.')
         return print()
     else:
-        print(f"{a}, ", end = '')
+        print(f"{a}, ", end='')
         if n > 1:
             a = b + c
-            fib(n-1, a = a, b = a, c = b)
-    
+            fib(n-1, a=a, b=a, c=b)
+
 
 fib(6)
 
@@ -38,7 +40,7 @@ fib(6)
 # Task 7.2.2 The same with the list:
 
 
-def fib(n, a = 0, b = 0, c = 1, l = []):
+def fib(n, a=0, b=0, c=1, l=[]):
     if a == 0:
         l.append(a)
     if n == 1 or n == 0:
@@ -46,8 +48,8 @@ def fib(n, a = 0, b = 0, c = 1, l = []):
     if n > 1:
         a = b + c
         l.append(a)
-        return fib(n-1, a = a, b = a, c = b, l = l)
-    
+        return fib(n-1, a=a, b=a, c=b, l=l)
+
 
 fib(7)
 
@@ -55,7 +57,7 @@ fib(7)
 # Task 7.2.3 Improving solution with cycle:
 
 
-def fib(n, a = 1, l = [0,1]):
+def fib(n, a=1, l=[0, 1]):
     if n == 1:
         return l[0]
     if a == n - 1:
@@ -63,7 +65,7 @@ def fib(n, a = 1, l = [0,1]):
     else:
         for i in range(a, a + 1):
             l.append(l[i-1] + l[i])
-            return fib(n, a + 1, l = l)
+            return fib(n, a + 1, l=l)
 
 
 fib(8)
@@ -72,13 +74,28 @@ fib(8)
 # Task 7.2.4 It's the best solution I can make at the moment:
 
 
-def fib(n, a = 0, b = 1, l = []):
+def fib(n, a=0, b=1, l=[]):
     if n == 0:
         return print(", ".join(map(str, l)))
     else:
         b += a
         l.append(a)
-        return fib(n - 1, a = b, b = a , l = l)
+        return fib(n - 1, a=b, b=a, l=l)
 
 
 fib(9)
+
+
+# Task 7.2.5 A bit upgrade:
+
+
+def fib(n, a = 0, b = 1):
+    if n == 0:
+        return
+    else:
+        b += a
+        print(a, end = ", " if n > 1 else ".")
+        return fib(n-1, a = b, b = a)
+
+
+fib(10)
