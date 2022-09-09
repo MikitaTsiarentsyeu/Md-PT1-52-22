104c6113e003c683800c5775dc32bf29dfb4aca3
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2022-09-09 19:57:37 +0600

    add lesson 09/09

--- Lessons/web project/proj/app/models.py
+++ Lessons/web project/proj/app/models.py
@@ -1,3 +1,21 @@
+from email.mime import image
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+
+    name = models.CharField(blank=False, max_length=100)
+    email = models.EmailField(blank=False, primary_key=True)
+
+class Post(models.Model):
+
+    POST_THEMES = [('n', 'nature'), ('t', 'technology'), ('s', 'sport')]
+
+    title = models.CharField(blank=False, max_length=100)
+    subtitle = models.CharField(max_length=200)
+    content = models.TextField(blank=False)
+    image = models.ImageField(upload_to='uploads')
+    issued = models.DateTimeField()
+    post_theme = models.CharField(choices=POST_THEMES, max_length=1, blank=False)
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+
